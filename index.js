f46192d4935d72768dbda76790f402699f49cfa8
Bennett Siegel <bennett.e.siegel@gmail.com>
2014-10-02 10:38:22 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+var mongoose = require('mongoose');
+var uristring = 'mongodb://oauth:oauth@linus.mongohq.com:10075/oauth';
+
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+    console.log ('Succeeded connected to: ' + uristring);
+  }
+});
+
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var oauthserver = require('oauth2-server');
+
+
+
+var app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.oauth = oauthserver({
+  model: require('./model'),
+  grants: ['authorization_code', 'password'],
+  debug: true
+});
+
+app.all('/oauth/token', app.oauth.grant());
+
+app.get('/', app.oauth.authorise(), function (req, res) {
+  res.send('Secret area');
+});
+
+app.use(app.oauth.errorHandler());
+
+app.listen(3000);
